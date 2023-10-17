95a9c4de558d6ec34bf911506fc48d5c171035c3
joaoaugusto-jesus <joaoaugustojesus@yahoo.com>
2023-10-17 16:26:21 +0100

    Terminated task_Generate Excuse

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#btm").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateExcuse = () => {
+  let pronoun = ["A", "The"];
+  let subject = ["jogger", "raccon", "dog", "driver", "comedian", "pincone"];
+  let action = ["took my", "threw my", "yelled at my", "sotle my", "bit my"];
+  let possetion = ["homework", "toe", "car", "shoe"];
+  let where = ["in the bed", "in my closet", "in my kitchen"];
+
+  let proIndx = Math.floor(Math.random() * pronoun.length);
+  let subjIndx = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possetionIndex = Math.floor(Math.random() * possetion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[proIndx] +
+    " " +
+    subject[subjIndx] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possetion[possetionIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
